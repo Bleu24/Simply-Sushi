80cfb4f5112242490ded65927864622fa4387407
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-19 03:36:47 +0800

    Remove sushi image component and associated code

--- src/components/sushi.js
+++
@@ -1,9 +0,0 @@
-import sushiImg from '../assets/img/sushi.png';
-
-export default function createImg() {
-    const img = document.createElement('img');
-    img.src = sushiImg;
-    img.classList.add('sushi-hero');
-    img.alt = 'A Sushi';
-    return img;
-}
