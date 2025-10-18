200730618ae39bec1d9cf71382813f821e56817d
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-19 03:36:27 +0800

    Refactor navigation button event handling and remove unused imports

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 import './styles.css';
 import 'normalize.css';
-import Sushi from './components/sushi.js'
 import Header from './components/header.js';
-import Content from './components/content.js';
+import Home from './components/home.js';
+import { state } from './utils/state.js';
+import { removeChildren, setComponentState } from './utils/componentFunctions.js';
+import renderTab from './utils/renderTab.js';
 
-Header();
-Content();
+Home();
 
+
+
+Header().addEventListener('click', e => {
+    const clickedBtn = e.target.closest('.nav__btn');
+    const allBtns = document.querySelectorAll('.nav__btn');
+
+    if (!clickedBtn) return;
+
+    allBtns.forEach(b => {
+        if (b === clickedBtn) {
+            if (b.dataset.state === 'active') return;
+            else {
+                setComponentState(b, 'active');
+                removeChildren(state.tab);
+                renderTab(b.dataset.tab);
+            }
+        } else {
+            b.removeAttribute('data-state');
+        }
+    });
+
+
+});
