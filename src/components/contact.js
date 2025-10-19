8a6b049698b282969a6c12948c3a217bb546817f
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-20 03:01:29 +0800

    Set active content to 'contact' in Contact component

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,7 @@
+import { state } from "../utils/state";
+
 export default function Contact() {
+    state.activeContent = 'contact';  
     const div = document.getElementById('content');
     const p = document.createElement('p');
     div.appendChild(p);
