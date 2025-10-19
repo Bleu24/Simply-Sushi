2bef65d45a6084e7b8792cbdd9038835e1da420c
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-20 03:01:26 +0800

    Set active content to 'about' in About component

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,7 @@
 import { state } from "../utils/state";
 
 export default function About() {
+    state.activeContent = 'about';  
     const div = document.getElementById('content');
     const p = document.createElement('p');
     div.appendChild(p);
