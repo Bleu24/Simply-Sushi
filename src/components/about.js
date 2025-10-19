7632876d5954f4013f811adfcc58a37369d5467b
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-19 22:21:55 +0800

    Add logging of active content in About component

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,11 @@
+import { state } from "../utils/state";
+
 export default function About() {
     const div = document.getElementById('content');
     const p = document.createElement('p');
     div.appendChild(p);
     p.textContent = 'About';
+    console.log(state.activeContent);
     return div;
+    
 }
