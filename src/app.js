509469b871480c70e6dde678a95eb2c3ce37b48f
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-20 01:28:13 +0800

    Replace Home component invocation with Menu component in app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import './styles/menu.css';
 import 'normalize.css';
 import Header from './components/header.js';
 import Home from './components/home.js';
+import Menu from './components/menu.js';
 import { state } from './utils/state.js';
 import { removeChildren, setComponentState } from './utils/componentFunctions.js';
 import renderTab from './utils/renderTab.js';
 import { animate, stagger } from 'motion';
 
-Home();
+Menu();
 
 
 
