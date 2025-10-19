908cebe73170fa2c0193602af871892eee9ce142
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-20 01:28:27 +0800

    Refactor Menu component to create sushi menu items dynamically

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,35 @@
 import { state } from "../utils/state";
+import Uramaki from '../assets/img/uramaki.png';
+import Nigiri from '../assets/img/uramaki.png';
 import { MenuItem } from "../classes/MenuItem";
 import createMenuItem from "../utils/createMenuItem";
 
 export default function Menu() {
     const div = document.getElementById('content');
-    const item1 = new MenuItem('fuck', 'Sushi', 200, 'スシ' ,'Sweet and Fragrant Sushi');
-    const itemEl = createMenuItem(item1);
 
-    div.appendChild(itemEl);
+    // create menu elements
+    const pageTitle = document.createElement('h1');
+    
+    // set classes
+    pageTitle.classList.add('menu__title');
+
+    // set texts
+    pageTitle.textContent = 'Check our finest Sushis';
+
+    //Sushi Data
+    const sushis = [
+        [Uramaki, 'Uramaki', 200, 'うらまき' ,'Sweet and Fragrant Uramaki'],
+        
+
+    ]
+
+    div.appendChild(pageTitle);
+    
+    sushis.forEach(sushiInfo => {
+        const sushi = new MenuItem(...sushiInfo);
+        const item = createMenuItem(sushi);
+        div.appendChild(item);
+    });
 
 
 
