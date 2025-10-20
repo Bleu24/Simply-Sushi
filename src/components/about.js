75fea047b5b5e6c6c9f133b2fddc8318462ba1e7
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-21 02:36:14 +0800

    Refactor About component to include aboutBodyText in content object

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,14 @@
 import { state } from "../utils/state";
+import { aboutBodyText } from "../misc";
 
 export default function About() {
-    state.activeContent = 'about';  
+    state.activeContent = 'about';
     const div = document.getElementById('content');
 
-    
-    
+    const content = {
+        heroPhoto: "Not Specified",
+        title: 'About Us',
+        body: aboutBodyText
+    };
+
 }
