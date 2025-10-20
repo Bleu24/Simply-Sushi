7d21892f3e4cb702a42c2c47bc4d9b71dff834b7
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-21 01:32:03 +0800

    Remove unused paragraph element and console log from About component

--- src/components/about.js
+++ src/components/about.js
@@ -3,10 +3,7 @@ import { state } from "../utils/state";
 export default function About() {
     state.activeContent = 'about';  
     const div = document.getElementById('content');
-    const p = document.createElement('p');
-    div.appendChild(p);
-    p.textContent = 'About';
-    console.log(state.activeContent);
-    return div;
+
+    
     
 }
