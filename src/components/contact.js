45dc00a0ea33cde9996da57e04714347deaa085d
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-22 00:17:07 +0800

    Refactor contact component to create and append form, ensuring required inputs and fade-in animation

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,18 @@
 import { state } from "../utils/state";
+import createForm from "../utils/createForm";
+import { animate, stagger } from "motion";
 
 export default function Contact() {
     state.activeContent = 'contact';  
     const div = document.getElementById('content');
-    const p = document.createElement('p');
-    div.appendChild(p);
-    p.textContent = 'Contact';
+    
+    const form = createForm();
+
+    form.classList.add('form');
+    
+    div.appendChild(form);
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(i => i.setAttribute('required', 'true'));
+    animate(form.childNodes, {opacity: 1}, {delay: stagger(0.1)} );
     return div;
 }
