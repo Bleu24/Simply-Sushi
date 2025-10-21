d66b1443f350fd63b856ed84be89f1a1a32fe12e
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-21 14:31:53 +0800

    Refactor createAbout utility to enhance element creation and structure

--- src/utils/createAbout.js
+++ src/utils/createAbout.js
@@ -1,5 +1,28 @@
 export default function (content) {
     // Create elements
     const div = document.createElement('div');
-    
+    const pageTitle = document.createElement('h1');
+    const img = new Image();
+    const text = document.createElement('p');
+    const body = document.createElement('div');
+
+    // Set up class names
+    div.classList.add('about__container');
+    pageTitle.classList.add('about__title');
+    img.classList.add('about__cover');
+    text.classList.add('about__text');
+    body.classList.add('about__body');
+
+    // Set up content
+    pageTitle.textContent = content.title;
+    img.src = content.cover;
+    text.innerHTML = content.body;
+
+    // Assemble elements
+    body.appendChild(pageTitle)
+    body.appendChild(text);
+    div.appendChild(img);
+    div.appendChild(body);
+
+    return div;
 }
