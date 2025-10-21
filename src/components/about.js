a07b44fb01b3193afbe7cdbfa9a6edac03583229
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-21 14:31:44 +0800

    Refactor About component to use cover image and createAbout utility

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,19 @@
 import { state } from "../utils/state";
 import { aboutBodyText } from "../misc";
+import RestoURL from "../assets/img/resto.png";
+import createAbout from "../utils/createAbout";
 
 export default function About() {
     state.activeContent = 'about';
     const div = document.getElementById('content');
 
     const content = {
-        heroPhoto: "Not Specified",
+        cover: RestoURL,
         title: 'About Us',
         body: aboutBodyText
     };
 
+    const aboutContent = createAbout(content);
+
+    div.appendChild(aboutContent);
 }
