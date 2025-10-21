9453391b004e5461b4d85bc6740b5c8210634b2e
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-21 17:07:51 +0800

    Add animation to fade in about content on load

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,7 @@ import { state } from "../utils/state";
 import { aboutBodyText } from "../misc";
 import RestoURL from "../assets/img/resto.png";
 import createAbout from "../utils/createAbout";
+import { animate, stagger } from "motion";
 
 export default function About() {
     state.activeContent = 'about';
@@ -15,5 +16,6 @@ export default function About() {
 
     const aboutContent = createAbout(content);
 
+    animate(aboutContent.childNodes, {opacity: 1}, {delay: stagger(0.1)} );
     div.appendChild(aboutContent);
 }
