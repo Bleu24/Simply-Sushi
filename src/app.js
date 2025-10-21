683f6fbafcd1d0c9703f2cafb9b5e5cf018758aa
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-22 00:16:59 +0800

    Import contact styles to enhance layout consistency across components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import './styles/menu.css';
 import './styles/about.css';
+import './styles/contact.css';
 import 'normalize.css';
 import loadContent from './utils/loadContent';
 
