497c118c8aef11733fbf41d7f699c4a77d3f5095
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-10-22 16:16:23 +0800

    Add initial webpack configuration for project setup with asset management

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff2|woff|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(mp3|wav|ogg|aac)$/i, // Match common audio file extensions
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Simply Sushi - シンプリー・スシ',
+            template: './src/app.html',
+        })
+    ],
+}
